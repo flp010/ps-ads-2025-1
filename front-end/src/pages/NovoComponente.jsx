c2a5327e02dee8b8f5f8c2d57522c4ceeb5ec4ba
Felipe <flp010@yahoo.com.br>
2025-04-05 19:15:28 -0300

    (19/03) Início da integração com front-end

---
+++ front-end/src/pages/NovoComponente.jsx
@@ -0,0 +1,64 @@
+import * as React from 'react';
+import Typography from "@mui/material/Typography";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import eu from '../assets/eu.jpg'
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+
+export default function NovoComponente() {
+    // Declarando uma variavel de estado usando o Lazy initializer
+    const [likes, setLikes] = React.useState(
+        () => window.localStorage.getItem('likes') ?? 0
+      )
+    // usando o useEffect para salvar o valor de likes no localStorage
+    React.useEffect(() => {
+        window.localStorage.setItem('likes', likes)
+      }, [likes])
+
+    return (
+        <>
+            {/* Título*/}
+            <Typography variant="h4" gutterBottom>Sobre o autor</Typography>
+
+            {/* Cartão*/}
+            <Card sx={{ maxWidth: 345 }}>
+
+                <CardMedia
+                    sx={{ height: 400 }}
+                    image={eu}
+                    title="eu"
+                />
+
+                <CardContent>
+                    {/* meu nome*/}
+                    <Typography gutterBottom variant="h5" component="div">
+                    Daniel Felipe de Sousa Rodrigues
+                    </Typography>
+
+                    {/* Texto sobre mim*/}
+                    <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                    Sou estudante de Análise e Desenvolvimento de Sistemas na Faculdade de 
+                    Tecnologia - Fatec Franca, com uma paixão intensa por tecnologia e inovação. 
+                    Estou ansioso para iniciar minha carreira e aplicar meus conhecimentos em um ambiente 
+                    profissional desafiador e colaborativo.
+                    </Typography>
+                </CardContent>
+                
+                <CardActions>
+                {/* Botão pra dar like*/}
+                <Button 
+            variant="contained" 
+            color="secondary" 
+            startIcon={<FavoriteBorderIcon />}
+            onClick={() => setLikes(Number(likes) + 1)}
+          >
+            CURTIR ({likes})
+          </Button>
+                </CardActions>
+            </Card>
+        </>
+    );
+}
