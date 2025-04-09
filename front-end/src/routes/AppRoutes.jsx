f1645c1bcafe3976ec74f76f386bebeb856e8cd3
Felipe <flp010@yahoo.com.br>
2025-04-09 10:59:49 -0300

    (09/04) Página de login front-end

--- front-end/src/routes/AppRoutes.jsx
+++ front-end/src/routes/AppRoutes.jsx
@@ -2,16 +2,20 @@ import { Routes, Route} from 'react-router-dom'
 
 import Homepage from '../pages/Homepage'
 
+import LoginPage from '../pages/LoginPage'
+
 import CustomersList from '../pages/customers/CustomersList'
 import CustomersForm from '../pages/customers/CustomersForm'
 import CarsList from '../pages/cars/CarsList'
 import CarsForm from '../pages/cars/CarsForm'
 import NovoComponente from '../pages/NovoComponente'
 
+
 export default function AppRoutes(){
     return (
         <Routes>
             <Route path="/" element = { <Homepage />} />
+            <Route path="/login" element = { <LoginPage />} />
             <Route path="/NovoComponente" element = { <NovoComponente/>} />
             <Route path="/customers" element = { <CustomersList />} />
             <Route path="/customers/new" element = { <CustomersForm />} />
@@ -19,6 +23,7 @@ export default function AppRoutes(){
             <Route path="/cars" element = { <CarsList />} />
             <Route path="/cars/new" element = { <CarsForm />} />
             <Route path="/cars/:id" element = { <CarsForm />} />
+
         </Routes>
     )
 }
